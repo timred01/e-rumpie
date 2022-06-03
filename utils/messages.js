deeee58ab75beb68610eb41db307aaed0ce0064e
Aip <32949229+timred01@users.noreply.github.com>
2022-06-03 17:35:23 +0700

    Update messages.js
    setting timezone jakarta

--- utils/messages.js
+++ utils/messages.js
@@ -1,10 +1,14 @@
 const moment = require('moment');
+const momentTz = require("moment-timezone");
+
+moment.locale("id");
+moment.tz.setDefault("Asia/Jakarta");
 
 function formatMessage(username, text) {
   return {
     username,
     text,
-    time: moment().format(' h:mm a')
+    time: moment().format(' HH:mm')
   };
 }
 
